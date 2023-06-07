73a4222e1cd5526ad40ab635225414d21e4b0ac3
Snehal Nalawade <97226192+Sneha-Nalawade@users.noreply.github.com>
2023-06-07 09:15:23 +0530

    Update set matrix zeroes.java
    code added for this problem

--- Day 1/set matrix zeroes.java
+++ Day 1/set matrix zeroes.java
@@ -1,1 +1,40 @@
+import java.io.*;
+import java.util.* ;
 
+public class Solution {
+    public static void setZeros(int matrix[][]) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[i][j]=-1;
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]==-1)
+                {
+                    matrix[i][j]=0;
+                    for(int k=0;k<m;k++)
+                    {
+                        if(matrix[i][k]!=-1)
+                         matrix[i][k]=0;
+                    }
+                    for(int k=0;k<n;k++)
+                    {
+                        if(matrix[k][j]!=-1)
+                         matrix[k][j]=0;
+                    }
+                }
+            }
+        }
+    }
+
+}
